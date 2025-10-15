17d9452bb56ba1282c0f648ebe011e6f3c948ba4
sachinkunwar <skkunwar04@gmail.com>
2025-10-16 00:10:06 +0530

    moved billing logic from billing controller to billing service

--- parkinglot/src/main/java/com/example/parkinglot/controller/BillController.java
+++ parkinglot/src/main/java/com/example/parkinglot/controller/BillController.java
@@ -19,19 +19,6 @@ public class BillController {
 	}
 
 	public Bill generateBill(Ticket ticket, Gate gate) {
-		double amount = 0;
-		LocalDateTime currentTime = LocalDateTime.now();
-		if(ticket.getParkingSpot().getVehicleType().equals(VehicleType.TWO_WHEELER)) {
-			amount = new TwoWheelerPricingStrategy().calculatePrice(ticket.getEntryTime(), currentTime);
-		}else if(ticket.getParkingSpot().getVehicleType().equals(VehicleType.FOUR_WHEELER)) {
-			amount = new FourWheelerPricingStrategy().calculatePrice(ticket.getEntryTime(), currentTime);
-		}
-		Bill bill = new Bill();
-		bill.setAmount(amount);
-		bill.setExitTime(currentTime);
-		bill.setTicket(ticket);
-		bill.setGate(gate);
-		bill = billService.generateBill(bill);
-		return bill;
+		return billService.generateBill(ticket, gate);
 	}
 }
--- parkinglot/src/main/java/com/example/parkinglot/service/BillService.java
+++ parkinglot/src/main/java/com/example/parkinglot/service/BillService.java
@@ -1,7 +1,14 @@
 package com.example.parkinglot.service;
 
+import java.time.LocalDateTime;
+
 import com.example.parkinglot.model.Bill;
+import com.example.parkinglot.model.Gate;
+import com.example.parkinglot.model.Ticket;
+import com.example.parkinglot.model.constant.VehicleType;
 import com.example.parkinglot.repository.BillRepository;
+import com.example.parkinglot.strategy.FourWheelerPricingStrategy;
+import com.example.parkinglot.strategy.TwoWheelerPricingStrategy;
 
 public class BillService {
 	private BillRepository billRepository;
@@ -10,7 +17,19 @@ public class BillService {
 		this.billRepository = billRepository;
 	}
 
-	public Bill generateBill(Bill bill) {
+	public Bill generateBill(Ticket ticket, Gate gate) {
+		double amount = 0;
+		LocalDateTime currentTime = LocalDateTime.now();
+		if(ticket.getParkingSpot().getVehicleType().equals(VehicleType.TWO_WHEELER)) {
+			amount = new TwoWheelerPricingStrategy().calculatePrice(ticket.getEntryTime(), currentTime);
+		}else if(ticket.getParkingSpot().getVehicleType().equals(VehicleType.FOUR_WHEELER)) {
+			amount = new FourWheelerPricingStrategy().calculatePrice(ticket.getEntryTime(), currentTime);
+		}
+		Bill bill = new Bill();
+		bill.setAmount(amount);
+		bill.setExitTime(currentTime);
+		bill.setTicket(ticket);
+		bill.setGate(gate);
 		return billRepository.save(bill);
 	}
 }
